d705552fd411d9f041ca51b47cfe49d8fe946291
797903 <54591322+797903@users.noreply.github.com>
2019-08-30 17:03:14 -0700

    Vectors

--- Lab829 Vectors/sketch.js
+++ Lab829 Vectors/sketch.js
@@ -1,25 +1,24 @@
 // Garrett Faure
 // 08/22
 var balls=[];
-var backgroundNum=150
 
 // setup runs once at the start of your program
 function setup(){
 // put start code here
-var cnv = createCanvas(800, 800);
+var cnv = createCanvas(1000, 800);
 cnv.position((windowWidth-width)/2, 30);
-background(20, 20, 20, backgroundNum);// background color
-// b1 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-// b2 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-// b3 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-// b4 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
+background(20, 20, 20);// background color
 loadBalls(50); // change class to loadBalls to equivalent
+// Draw FPS (rounded to 2 decimal places) at the bottom left of the screen
+let fps = frameRate();
+fill(255);
+stroke(0);
+text("FPS: " + fps.toFixed(2), 10, height - 10);
 }
-
 
 // Draw runs 30 times a second
 function draw() {
-  background(20, 20, 20, backgroundNum);
+  background(20, 20, 20);
   runBalls();
 }
 
