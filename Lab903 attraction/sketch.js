5a52e5b66a2614eaafc589318401965736463262
797903 <54591322+797903@users.noreply.github.com>
2019-09-03 17:21:55 -0700

    Attraction minus the attraction part

--- Lab903 attraction/sketch.js
+++ Lab903 attraction/sketch.js
@@ -1,12 +1,12 @@
 // Garrett Faure
-// 08/22
+// 09/03
 var balls=[];
 var bigFella;
 
 // setup runs once at the start of your program
 function setup(){
 // put start code here
-var cnv = createCanvas(1000, 800);
+var cnv = createCanvas(800, 800);
 cnv.position((windowWidth-width)/2, 30);
 background(20, 20, 20);// background color
 loadBalls(50); // change class to loadBalls to equivalent
@@ -17,21 +17,22 @@ stroke(0);
 text("FPS: " + fps.toFixed(2), 10, height - 10);
 }
 
-// Draw runs 30 times a second
-function draw() {
-  background(20, 20, 20);
-  runBalls();
-}
-
 function loadBalls(n){
-  bigFella = new Ball(random(width), random(height), random(2, -2), random(2, -2), -1)
+  bigFella = new Ball(width/2, height/2, random(-1, 1), random(-1, 1), 1)
   for(var i = 0; i < n; i++){
-    balls[i] = new Ball(random(width), random(height), .5, .5)
+    balls[i] = new Ball(random(width), random(height), .5, .5, i)
   }
 }
 
 function runBalls(){
+  bigFella.run();
   for(var i = 0; i < balls.length; i++){
     balls[i].run();
   }
+}
+
+// Draw runs 30 times a second
+function draw() {
+  background(20, 20, 20);
+  runBalls();
 }
