018f456d1b5577588eb5ed4fa228cc19787ac9b0
797903 <54591322+797903@users.noreply.github.com>
2019-09-06 08:49:42 -0700

    Rotation

---
+++ Lab906 Rotation/sketch.js
@@ -0,0 +1,38 @@
+// Garrett Faure
+// 09/06
+var balls = [];
+var bigFella;
+
+// setup runs once at the start of your program
+function setup(){
+// put start code here
+var cnv = createCanvas(800, 800);
+cnv.position((windowWidth-width)/2, 30);
+background(20, 20, 20);// background color
+loadBalls(2000); // change class to loadBalls to equivalent
+// Draw FPS (rounded to 2 decimal places) at the bottom left of the screen
+// let fps = frameRate();
+// fill(255);
+// stroke(0);
+// text("FPS: " + fps.toFixed(2), 10, height - 10);
+}
+
+function loadBalls(n){
+  bigFella = new Ball(width/2, height/2, random(-2.5, 2.5), random(-2.5, 2.5), -1)
+  for(var i = 0; i < n; i++){
+    balls[i] = new Ball(random(width), random(height), 3, 3, i)
+  }
+}
+
+function runBalls(){
+  bigFella.run();
+  for(var i = 0; i < balls.length; i++){
+    balls[i].run();
+  }
+}
+
+// Draw runs 30 times a second
+function draw() {
+  background(20, 20, 20);
+  runBalls();
+}
