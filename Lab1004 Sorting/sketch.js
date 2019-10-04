c9330c039a3bcf64b5d7197837fc1ad2aba1c8f0
797903 <54591322+797903@users.noreply.github.com>
2019-10-04 08:48:06 -0700

    new

---
+++ Lab1004 Sorting/sketch.js
@@ -0,0 +1,16 @@
+// Garrett Faure
+// 10/04
+var a
+var b
+var temp
+
+}
+
+// setup runs once at the start of your program
+function setup(){
+// put start code here
+var cnv = createCanvas(800, 800);
+cnv.position((windowWidth-width)/2, 30);
+background(255, 255, 255);// background color
+
+}
