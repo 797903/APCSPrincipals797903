6e156e055ad3f872b637f334104bfd7639bbe225
797903 <54591322+797903@users.noreply.github.com>
2019-10-09 07:45:16 -0700

    checkerboard

---
+++ Lab1007 Checkerboard/sketch.js
@@ -0,0 +1,12 @@
+// Garrett Faure
+// 10/07
+
+
+// setup runs once at the start of your program
+function setup(){
+// put start code here
+var cnv = createCanvas(800, 800);
+cnv.position((windowWidth-width)/2, 30);
+background(200, 200, 200);// background color
+
+}
