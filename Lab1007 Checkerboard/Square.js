64262fb1128953a89c032ba9540ef4b599ef1ec7
797903 <54591322+797903@users.noreply.github.com>
2019-10-15 08:47:21 -0700

    adf

--- Lab1007 Checkerboard/Square.js
+++ Lab1007 Checkerboard/Square.js
@@ -1,5 +1,17 @@
-Constructor(x, y, clr){
-  this.loc = createVector(x, y)
-  this.clr = clr
+class Square{
+  Constructor(x, y, clr){
+    this.loc = createVector(x, y);
+    this.clr = clr;
+    this.x = x;
+    this.y = y;
+  }
 
+  run(){
+    this.render()
+  }
+
+  render(){
+    fill(this.clr)
+    rect(this.x, this.y, 20, 20)
+  }
 }
