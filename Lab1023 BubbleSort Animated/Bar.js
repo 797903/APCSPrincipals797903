3e64bb9fedf65d0071d466b53c945229f53d4023
797903 <54591322+797903@users.noreply.github.com>
2019-10-24 08:51:01 -0700

    lots of code

--- Lab1023 BubbleSort Animated/Bar.js
+++ Lab1023 BubbleSort Animated/Bar.js
@@ -1,8 +1,27 @@
+// class Bar{
+//   constructor(x, y, w, h, clr){
+//     this.loc = createVector(x, y);
+//     this.w = w;
+//     this.h = h;
+//   }
+// }
+//
+// render(){
+//   fill(0, 100, 200);
+//   rect(this.loc.x, this.loc.y, this.w, this.h);
+// }
+
 class Bar{
   constructor(x, y, w, h, clr){
-    this.loc = createVector(x, y);
-    this.w = w;
-    this.h = h;
-    this.clr = color(random(0, 255), random(0, 255), random(0, 255));
+    this.loc=createVector(x,y);
+    this.w=w;
+    this.h=h;
+  }
+run(){
+  this.render();
+
 }
+render(){
+  fill(0,255,255);
+  rect(this.loc.x,this.loc.y,this.w,this.h);
 }
--- Lab1023 BubbleSort Animated/sketch.js
+++ Lab1023 BubbleSort Animated/sketch.js
@@ -1,34 +1,82 @@
 // Garrett Faure
 // 10/04
-var list = [];
-
-// setup runs once at the start of your program
+var bars=[];
+n = 100
+//put setup code here
 function setup(){
-// put start code here
-var cnv = createCanvas(800, 800);
-cnv.position((windowWidth-width)/2, 30);
-background(200, 200, 200);// background color
-
-loadNum(10);
-bubbleSort(list);
-
-}
-
-function loadNum(n){
-  for(i = 0; i <= n; i++){
-    list[i] = int(random(0, n));
+  var cnv=createCanvas(800,800);
+  cnv.position((windowWidth-width)/2,30);
+  background(200);
+  loadBars(n);
+  for(var i=0; i<bars.length; i++){
+    bars[i].run();
   }
 }
 
-function bubbleSort(list){
-  for(var j = list.length; j > 0 ; j --){
-      for (var i = 0 ; i < list.length; i ++){
-          if (list[i+1] < list [i]){
-            var temp = list[i];
-            list[i] = list[i+1];
-            list[i+1] = temp;
-          }
-        }
+function draw(){
+  bubbleSort()
+}
+function move(){
+  for(var i=0; i < bars.length; i++){
+    bars[i].set(i);
+  }
+}
+
+function bubbleSort(){
+  for (var i = bars.length-1; i > 0; i--){
+    for (var j = 0; j < i; j++){
+      if(bars[j].h > bars[j + 1].h){
+        swap(bars, j, j+1);
       }
-      console.log(list);
     }
+  }
+}
+
+function loadBars(){
+  for (var i = 0; i < 32; i++){
+    var j = int(random(n));
+    bars[i]= new Bar(25 * i, 800 - (25 * j), 25, (25 * j))
+  }
+}
+
+function swap(list, a, b){
+  var temp = list[a];
+  list[a] = list[b];
+  list[b] = temp;
+}
+
+// var list = [];
+//
+// // setup runs once at the start of your program
+// function setup(){
+// // put start code here
+// var cnv = createCanvas(800, 800);
+// cnv.position((windowWidth-width)/2, 30);
+// background(200, 200, 200);// background color
+//
+// loadNum(10);
+// bubbleSort(list);
+//
+// }
+//
+// function draw(){
+//   for(var i = 0; )
+// }
+//
+// function loadNum(n){
+//   for(i = 0; i <= n; i++){
+//     list[i] = int(random(0, n));
+//   }
+// }
+//
+// function bubbleSort(list){
+//   for(var j = list.length; j > 0 ; j --){
+//       for (var i = 0 ; i < list.length; i ++){
+//           if (list[i+1] < list [i]){
+//             var temp = list[i];
+//             list[i] = list[i+1];
+//             list[i+1] = temp;
+//           }
+//         }
+//       }
+//     }
