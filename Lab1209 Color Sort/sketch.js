e8807c89705b8da97acae916ad09800720ebc3c9
797903 <54591322+797903@users.noreply.github.com>
2019-12-09 13:07:05 -0800

    red

--- Lab1209 Color Sort/sketch.js
+++ Lab1209 Color Sort/sketch.js
@@ -1,6 +1,7 @@
 // Garrett Faure
 // 08/22
 var balls=[];
+var n = 20;
 
 // setup runs once at the start of your program
 function setup(){
@@ -8,14 +9,15 @@ function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);// background color
-  loadBalls(20); // change class to loadBalls to equivalent
+  loadBalls(n); // change class to loadBalls to equivalent
+  frameRate(5);
 }
 
 
 // Draw runs 30 times a second
 function draw() {
-  for(var i=0; i<balls.length-1; i++){
-    if(balls[i].getR()>balls[i+1].getR()){
+  for(var i = 0; i < (balls.length - 1); i++){
+    if(balls[i].getR() > balls[i + 1].getR()){
       balls[i].x = 35*i+75;
       balls[i].render();
       swap(i,i+1);
@@ -25,7 +27,7 @@ function draw() {
 
 function swap(a,b){
 temp = balls[a];
-balls[a]=balls[b];
+balls[a] = balls[b];
 balls[b] = temp;
 }
 
@@ -38,20 +40,5 @@ function loadBalls(n){
 function runBalls(){
   for(var i = 0; i < balls.length; i++){
     balls[i].run();
-  }
-}
-
-function selectionSort(balls){
-  var min, temp, len = balls.length;
-  for(var i = 0; i < len; i++){
-    min = i;
-    for(var q = i + 1; q < len; q++){
-      if(balls[q] < balls[min]){
-        min = q;
-      }
-    }
-    temp = balls[i]; // swap after inner loop
-    balls[i] = balls[min];
-    balls[min] = temp;
   }
 }
